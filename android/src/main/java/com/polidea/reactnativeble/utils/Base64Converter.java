e3a74eb0aaeb9454d9eb5696535c65c9664059ff
yoppoy <yan.poinssot@epitech.eu>
2019-01-29 21:30:18 +0800

    Update Base64Converter.java

--- android/src/main/java/com/polidea/reactnativeble/utils/Base64Converter.java
+++ android/src/main/java/com/polidea/reactnativeble/utils/Base64Converter.java
@@ -4,9 +4,9 @@ import android.util.Base64;
 
 public class Base64Converter {
     public static String encode(byte[] bytes) {
-        return Base64.encodeToString(bytes, Base64.NO_WRAP);
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
     }
     public static byte[] decode(String base64) {
-        return Base64.decode(base64, Base64.NO_WRAP);
+        return Base64.decode(base64, Base64.DEFAULT);
     }
 }
